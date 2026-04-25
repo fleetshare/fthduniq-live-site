7c9a726fd81e8afa62393c0c3c07fb60c307002d
Faith Onyenwenu <diamondrejoice@gmail.com>
2026-04-25 10:57:04 +0100

    Create protect.js

---
+++ protect.js
@@ -0,0 +1,11 @@
+document.addEventListener("contextmenu", function(e){
+  e.preventDefault();
+});
+
+document.addEventListener("copy", function(e){
+  e.preventDefault();
+});
+
+document.addEventListener("dragstart", function(e){
+  e.preventDefault();
+});
